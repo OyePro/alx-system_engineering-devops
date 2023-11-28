b86208524c71edcace8c75321a81255d6cb9d30c
Olalekan Okewale <olalekanokewale2020@gmail.com>
2023-11-28 15:30:57 +0100

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
 # Using Puppet, install flask from pip3 with version of `2.1.0`
 
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
-  require  =>  Package['python3-pip']
+  provider => 'pip3',
+  require  =>  Package['werkzeug'],
 }
